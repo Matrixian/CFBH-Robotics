ec3b7bedff44f58facbae5725514c17ee9124756
Holger Rasmussen <h.rasmussen@gmx.net>
2011-11-11 16:59:23 +0800

    Initial Project

---
+++ Assignment3b/src/Assignment3b.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Assignment3b.cpp
+// Author      : Cristian, Holger, Barbara, Felix
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
